1166f64ace7c34ee0eb418c6e3bbb0629a132ec1
Facundo Chaud <faku_imper@hotmail.com.ar>
2025-12-10 18:40:39 -0300

    is loading in cashier orders buttons

--- app/cashier/orders.tsx
+++ app/cashier/orders.tsx
@@ -4,10 +4,60 @@ import { useRouter } from "expo-router";
 
 import { OrderListing } from "@/components/OrderListing";
 import { useApi } from "@/hooks/useApi";
-import { Order } from "@/types";
+import { Order, OrderStatus } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+
+const ActionButton = ({
+  key,
+  text,
+  order,
+  targetStatus,
+  onFinishAction,
+}: {
+  key: string;
+  text: string;
+  order: Order;
+  targetStatus: OrderStatus;
+  onFinishAction: () => void;
+}) => {
+  const { updateOrderStatus } = useApi();
+
+  const updateOrderStatusMutation = useMutation({
+    mutationFn: ({
+      order,
+      orderStatus,
+    }: {
+      order: Order;
+      orderStatus: OrderStatus;
+    }) => updateOrderStatus({ orderId: order.id, orderStatus: orderStatus }),
+    onSuccess: () => {
+      onFinishAction();
+    },
+    onError: (error: unknown) => {
+      console.error(error);
+      onFinishAction();
+    },
+  });
+
+  return (
+    <Button
+      key={key}
+      mode="contained"
+      onPress={() =>
+        updateOrderStatusMutation.mutate({
+          order: order,
+          orderStatus: targetStatus,
+        })
+      }
+      loading={updateOrderStatusMutation.isPending}
+      disabled={updateOrderStatusMutation.isPending}
+    >
+      {text}
+    </Button>
+  );
+};
 
 const CashierOrders = () => {
-  const { updateOrderStatus } = useApi();
   const router = useRouter();
 
   const getActions = (
@@ -18,30 +68,20 @@ const CashierOrders = () => {
     if (!order || order.last_status?.status != "PLACED") return [];
 
     return [
-      <Button
-        mode="contained"
-        key="approve"
-        onPress={() =>
-          updateOrderStatus({
-            orderId: order.id,
-            orderStatus: "ACCEPTED",
-          }).then(onActionClose)
-        }
-      >
-        Aprobar
-      </Button>,
-      <Button
-        mode="contained"
-        key="reject"
-        onPress={() =>
-          updateOrderStatus({
-            orderId: order.id,
-            orderStatus: "REJECTED",
-          }).then(onActionClose)
-        }
-      >
-        Rechazar
-      </Button>,
+      <ActionButton
+        key={"approve"}
+        text={"Aprobar"}
+        order={order}
+        targetStatus={"ACCEPTED"}
+        onFinishAction={onActionClose}
+      />,
+      <ActionButton
+        key={"reject"}
+        text={"Rechazar"}
+        order={order}
+        targetStatus={"REJECTED"}
+        onFinishAction={onActionClose}
+      />,
     ];
   };
 
